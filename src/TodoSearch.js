c83085539d304190575ceec8d58a37ebbba89972
Jonathan Mauricio Forero Enciso <jonathan.forero@globant.com>
2022-04-05 18:49:05 -0500

    add: hamdling state with useState hook

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
 
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
-    return (
+    return [
         <input
             className="TodoSearch"
             placeholder="Type here..."
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
