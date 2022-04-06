6479be62c095248b99cc93827a637981dc446400
Jonathan Mauricio Forero Enciso <jonathan.forero@globant.com>
2022-04-05 19:58:23 -0500

    add: counter and filter

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './TodoCounter.css';
 
-function TodoCounter() {
+function TodoCounter({ total, completed }) {
     return (
-        <h2 className="TodoCounter">You have been completed 2 of 3 TODOS</h2>
+        <h2 className="TodoCounter">You have been completed {completed} of {total} TODOS</h2>
     );
 }
 
